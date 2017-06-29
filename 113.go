c463d84f45129f0d5385da9ca7985561054c0c7e
ybw139 <1392802783@qq.com>
2017-06-29 10:17:27 +0800

    新增113.go

---
+++ 113.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/astaxie/beego"
+	"github.com/hprose/hprose-golang/rpc"
+	ws "github.com/hprose/hprose-golang/rpc/websocket"
+	_ "test.com/test/routers"
+)
+
+func hello(name string) string {
+	return "Hello " + name + "!"
+}
+func runWebSocket() {
+	service := ws.NewWebSocketService()
+	service.AddFunction("hello", hello)
+	http.ListenAndServe(":8081", service)
+	// http.ListenAndServeTLS(":8081", beego.AppConfig.String("HTTPSCertFile"), beego.AppConfig.String("HTTPSKeyFile"), service)
+}
+func main() {
+	go runWebSocket()
+	service := rpc.NewHTTPService()
+	service.AddFunction("hello", hello)
+	beego.Handler("/rpc", service)
+	beego.Run()
+}
